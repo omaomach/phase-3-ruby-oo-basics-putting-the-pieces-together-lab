3a0a8b42d74d4fd7b80ee1ea49707fa2444c9824
Machoka Joash Omao <aaronjoash44@gmail.com>
2022-10-20 14:41:53 +0300

    completed phase 3 oo basics putting the pieces together lab

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,24 @@
+class Book
+    attr_reader :title
+    attr_accessor :author, :page_count, :genre
 
+    def initialize title
+        @title = title
+    end
 
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+
+end
+
+book = Book.new("Harry Porter")
+book.author = "J.K Rowling"
+book.page_count = "697"
+book.genre = "Fiction"
+
+puts book.title
+puts book.author
+puts book.page_count
+puts book.genre
+book.turn_page
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,28 @@
-# Make your shoe class here!
+class Shoe
+    attr_reader :brand
+    attr_accessor :color, :size, :material, :condition
+
+    def initialize brand
+        @brand = brand
+    end
+
+    def cobble
+        puts "Your shoe is as good as new!"
+        @condition = "new"
+    end
+
+end
+
+shoe1 = Shoe.new("Bata")
+shoe1.color = ("black")
+shoe1.size = ("12")
+shoe1.material = ("leather")
+shoe1.condition = ("old")
+
+puts shoe1.brand
+puts shoe1.color
+puts shoe1.size
+puts shoe1.material
+puts shoe1.condition
+shoe1.cobble
+puts shoe1.condition
